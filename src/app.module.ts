0e98541abbcd06b2c5eb6f54e07124e1535470c1
AntonBrito <tonybrito85@gmail.com>
2021-06-21 17:58:13 -0400

    Added dive-services to app-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DivesController } from './dives/dives.controller';
+import { DivesService } from './dives/dives.service';
 
 @Module({
   imports: [],
   controllers: [AppController, DivesController],
-  providers: [AppService],
+  providers: [AppService, DivesService],
 })
 export class AppModule {}
