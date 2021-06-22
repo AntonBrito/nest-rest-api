0588c78ceb92719d39a270e70fc3cb6cea106a8e
AntonBrito <tonybrito85@gmail.com>
2021-06-22 00:39:56 -0400

    Implemented svelt with nest.preconfig the view engine

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { svelteViewEngine } from './svelte-view-engine';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.engine('svelte', svelteViewEngine);
+  app.setViewEngine('svelte');
+
   await app.listen(3000);
 }
 bootstrap();
