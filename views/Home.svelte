2f0321606dd47f5116cd229726bfcc9270d597ba
AntonBrito <tonybrito85@gmail.com>
2021-06-22 02:29:37 -0400

    Created a home view component

---
+++ views/Home.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Layout from  '/Layout/svelte';
+  export let message
+</script>
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+<Layout>
+  <h1>{message}</h1>
+</Layout>
+<style>
+  h1 {
+    color: pruple;
+  }
+  :global(body) {
+    background-color: pink
+  }
+  </style>
