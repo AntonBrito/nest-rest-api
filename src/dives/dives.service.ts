3909047064bdd2c7e1877fa0fea3a84d56854dc1
AntonBrito <tonybrito85@gmail.com>
2021-06-21 21:22:32 -0400

    Implemtenteddive interface and some hard code

--- src/dives/dives.service.ts
+++ src/dives/dives.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Dive } from './interfaces/dive.interface';
 
 @Injectable()
-export class DivesService {}
+export class DivesService {
+  private readonly dive: Dive[] = [
+    {
+      id: "5938395976",
+      name: "John Duatez",
+      location: "KeyWest",
+      depth: 10,
+    }
+  ]
+}
---
+++ src/dives/interfaces/dive.interface.ts
@@ -0,0 +1,6 @@
+export interface Dive {
+  id?: string;
+  name: string;
+  location: string;
+  depth: number;
+}
