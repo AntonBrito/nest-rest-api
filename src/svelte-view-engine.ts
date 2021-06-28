15ce5ad25fec8a26daf98a256c7e310bbc3e8396
AntonBrito <tonybrito85@gmail.com>
2021-06-28 02:18:34 -0400

    Fixed a typo, modifyed the the view component

--- src/svelte-view-engine.ts
+++ src/svelte-view-engine.ts
@@ -1,7 +1,10 @@
- import 'svelte/register';
+import { identity } from 'rxjs';
 
 export function svelteViewEngine(filePath: string, options: any, next) {
   const Component = require(filePath).default;
-  const { html } = Component.render(options);
-  next(null, html);
+  let { html, head, css } = Component.render(options);
+  if(css.code) {
+    head = `${head}<style>${css.code}</style>`;
+  }
+  next(null, html.replace('%head%', head));
 }
