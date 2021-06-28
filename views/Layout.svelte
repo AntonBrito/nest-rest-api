fabc1ac41a65bc0a29602e4803c57d13c7a0c4a4
AntonBrito <tonybrito85@gmail.com>
2021-06-28 02:16:58 -0400

    Added Layout component

--- views/Layout.svelte
+++ views/Layout.svelte
@@ -1,10 +1,28 @@
+<script>
+  import Nav from './Nav.svelte';
+</script>
+
+<!doctype html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=rdge" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width-device-width, initial-scale=1.0" />
+    <meta name="description" content="nestjs app with svelte template" />
+    %head%
   </head>
   <body>
-    <slot />
+    <header>
+      <Nav />
+    </header>
+    <main>
+     
+    </main>
   </body>
 </html>
+
+<style>
+  :global(body) {
+    background-color: white;
+  }
+</style>
