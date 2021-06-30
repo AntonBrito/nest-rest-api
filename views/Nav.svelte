7485638ccd86ad78cc21fc1dbb5abb3402cb82a1
AntonBrito <tonybrito85@gmail.com>
2021-06-29 23:15:49 -0400

    Implemented style to navbar

--- views/Nav.svelte
+++ views/Nav.svelte
@@ -1,9 +1,41 @@
+<script>
+const { default: Home }=require("./Home.svelte");
+
+</script>
 <div class="navbar bg-primary">
   <h1>ScubaLog</h1>
+  <!-- <nav>
+    <ul>
+      <a class=""src="About">About</a>
+    </ul>
+  </nav> -->
 </div>
 
+
 <style>
-  h1 {
+  /* h1 {
     text-align: center;
-  }
+  } */
+
+  .navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.7rem 2rem;
+  z-index: 1;
+  width: 100%;
+  border-bottom: #204f6e;
+  opacity: 0.9;
+  margin-bottom: 1rem;
+} 
+
+.bg-primary {
+  background: #204f6e;
+  color: #fff;
+}
+
+.navbar h1 {
+  margin-bottom: 0.6rem;
+  margin-right: 0.6rem;
+}
 </style>
