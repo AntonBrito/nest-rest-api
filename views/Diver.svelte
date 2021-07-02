1b133622e29572cafe637909718e6ccca43ad99d
AntonBrito <tonybrito85@gmail.com>
2021-07-02 01:51:11 -0400

    Added diver re-organizing file strucure

---
+++ views/Diver.svelte
@@ -0,0 +1,31 @@
+<script>
+ export let name;
+ export let location;
+ export let depths;
+
+</script>
+
+      <div class="card">
+        <h1>{name}</h1>
+        <h3>{location}</h3>
+        <h3>{depths}</h3>
+    </div>
+  
+<style>
+ 
+h1 {
+  color: black;
+}
+
+h3 {
+  color: #204f6e;
+  margin-bottom: 10px;;
+}
+
+.card {
+  padding: 1rem;
+  border: #ccc 1px dotted;
+  margin: 0.7rem 0;
+}
+
+</style>
