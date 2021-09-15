13f2debbe6622f483d68db877d47a8d1411cb4cf
AntonBrito <tonybrito85@gmail.com>
2021-09-15 12:29:40 -0400

    Working on forms for  creating dive logs

---
+++ views/CreateDive.svelte
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+<form>
+  <input type='text' required bind:value={log} />
+  <buttom type='submit' on:click|preventDefault={submitLog}>Submit</buttom>
+</form>
+<style>
+
+</style>
