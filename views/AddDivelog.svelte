3fac8c43f9b3e40ac2c8a01e6fd82b954396ae2e
AntonBrito <tonybrito85@gmail.com>
2021-09-16 23:52:03 -0400

    Created addlog form

---
+++ views/AddDivelog.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+<form>
+  <label for="name">Name</label>
+  <input id="name" />
+  <label for="location">Location</label>
+  <input id="location" />
+  <input placeholder="Depth"id="depth" />
+  <button id="sbmit">Submit</button>
+
+</form>
+<style>
+
+</style>
